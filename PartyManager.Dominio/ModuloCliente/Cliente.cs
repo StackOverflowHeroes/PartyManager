d1fb35b7f65765bd7ee225fd72a37f647f328435
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-06-12 17:52:24 -0300

    Refatoração IRepositorioCliente e Implementação Atributo contadorFesta em Cliente

--- PartyManager.Dominio/ModuloCliente/Cliente.cs
+++ PartyManager.Dominio/ModuloCliente/Cliente.cs
@@ -4,18 +4,12 @@ namespace PartyManager.Dominio.ModuloCliente
 {
      public class Cliente : EntidadeBase<Cliente>
      {
-          public string nome { get; set;}
+          public string nome { get; set; }
           public string telefone;
-
+          public int contadorFesta = 0;
           public Cliente()
           {
 
-          }
-
-          public Cliente(string nome, string telefone)
-          {
-               this.nome = nome;
-               this.telefone = telefone;
           }
           public Cliente(int id, string nome, string telefone)
           {
@@ -48,7 +42,8 @@ namespace PartyManager.Dominio.ModuloCliente
                return obj is Cliente cliente &&
                       id == cliente.id &&
                       nome == cliente.nome &&
-                      telefone == cliente.telefone;
+                      telefone == cliente.telefone &&
+                      contadorFesta == cliente.contadorFesta;
           }
      }
 }
