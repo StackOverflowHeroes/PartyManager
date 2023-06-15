b8a1a9751338b89efdad16c7714b1ab0a22c81ac
Leonardo <contato.leoo0@gmail.com>
2023-06-15 15:30:15 -0300

    styles: removido alguns componentes da tela tema

--- PartyManager.WinApp/ModuloTema/TelaTemaForm.cs
+++ PartyManager.WinApp/ModuloTema/TelaTemaForm.cs
@@ -4,94 +4,94 @@ using PartyManager.Dominio.ModuloTema;
 namespace PartyManager.WinApp.ModuloTema
 {
     public partial class TelaTemaForm : Form
-     {
-          List<Item> ListaItensTema = new List<Item>();
-          List<string> erros = new List<string>();
-          public TelaTemaForm()
-          {
-               InitializeComponent();
-               this.ConfigurarDialog();
-          }
+    {
+        List<Item> ListaItensTema = new List<Item>();
+        List<string> erros = new List<string>();
+        public TelaTemaForm()
+        {
+            InitializeComponent();
+            this.ConfigurarDialog();
+        }
 
-          public Tema ObterTema()
-          {
-               int id = Convert.ToInt32(tboxId.Text);
-               string nome = tboxNome.Text;
-               List<Item> lista = ListaItensTema;
+        public Tema ObterTema()
+        {
+            int id = Convert.ToInt32(tboxId.Text);
+            string nome = tboxNome.Text;
+            List<Item> lista = ListaItensTema;
 
-               Tema tema = new Tema(id, nome, lista);
+            Tema tema = new Tema(id, nome, lista);
 
-               if (id > 0)
-                    tema.id = id;
+            if (id > 0)
+                tema.id = id;
 
-               return tema;
-          }
+            return tema;
+        }
 
-          public void ConfigurarTela(Tema tema)
-          {
-               tboxId.Text = tema.id.ToString();
-               tboxNome.Text = tema.nome;
+        public void ConfigurarTela(Tema tema)
+        {
+            tboxId.Text = tema.id.ToString();
+            tboxNome.Text = tema.nome;
 
-               foreach (Item registro in tema.ListaItens)
-               {
-                    ListBoxItens.Items.Add(registro);
-                    ListaItensTema.Add(registro);
-               }
-          }
+            foreach (Item registro in tema.ListaItens)
+            {
+                ListBoxItens.Items.Add(registro);
+                ListaItensTema.Add(registro);
+            }
+        }
 
-          private void btnCadastrar_Click(object sender, EventArgs e)
-          {
-               Tema tema = ObterTema();
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            Tema tema = ObterTema();
 
-               string[] erros = tema.ValidarErros(); 
+            string[] erros = tema.ValidarErros();
 
-               if (erros.Length > 0)
-               {
-                    TelaPrincipalForm.Instancia.AtualizarRodape(erros[0], TipoStatusEnum.Erro);
-                    DialogResult = DialogResult.None;
-               }
-          }
+            if (erros.Length > 0)
+            {
+                TelaPrincipalForm.Instancia.AtualizarRodape(erros[0], TipoStatusEnum.Erro);
+                DialogResult = DialogResult.None;
+            }
+        }
 
-          private void btnAdicionarItem_Click(object sender, EventArgs e)
-          {
-               erros.Clear();
-               int id = Convert.ToInt32(tboxId.Text);
-               string nome = txtBoxNomeItem.Text;
-               decimal valor = 0;
-              
-               try
-               {
-                    valor = Convert.ToDecimal(txtboxValorItem.Text);
-               }
-               catch (FormatException)
-               {
-                    erros.Add("O campo \"Valor\" em itens deve ser numérico!");
-               }
+        private void btnAdicionarItem_Click(object sender, EventArgs e)
+        {
+            erros.Clear();
+            int id = Convert.ToInt32(tboxId.Text);
+            string nome = txtBoxNomeItem.Text;
+            decimal valor = 0;
 
-               Item novoItem = new Item(id, nome, valor);
+            try
+            {
+                valor = Convert.ToDecimal(txtboxValorItem.Text);
+            }
+            catch (FormatException)
+            {
+                erros.Add("O campo \"Valor\" em itens deve ser numérico!");
+            }
 
-               erros.AddRange(novoItem.ValidarErros());
+            Item novoItem = new Item(id, nome, valor);
 
-               if (erros.Count > 0)
-               {
-                    TelaPrincipalForm.Instancia.AtualizarRodape(erros[0], TipoStatusEnum.Erro);
-                    return;
-               }
+            erros.AddRange(novoItem.ValidarErros());
 
-               ListaItensTema.Add(novoItem);
+            if (erros.Count > 0)
+            {
+                TelaPrincipalForm.Instancia.AtualizarRodape(erros[0], TipoStatusEnum.Erro);
+                return;
+            }
 
-               txtBoxNomeItem.Text = "";
-               txtboxValorItem.Text = "";
+            ListaItensTema.Add(novoItem);
 
-               ListBoxItens.Items.Add(novoItem);
-          }
+            txtBoxNomeItem.Text = "";
+            txtboxValorItem.Text = "";
 
-          private void button1_Click(object sender, EventArgs e)
-          {
-               Item item = ListBoxItens.SelectedItem as Item;
-               ListBoxItens.Items.Remove(item);
-               ListaItensTema.Remove(item);
-          }
-     }
+            ListBoxItens.Items.Add(novoItem);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Item item = ListBoxItens.SelectedItem as Item;
+            ListBoxItens.Items.Remove(item);
+            ListaItensTema.Remove(item);
+        }
+    }
 }
 
