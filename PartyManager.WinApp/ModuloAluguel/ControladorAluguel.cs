4378261574d7cb4f2e7677c563797407c865193c
Leonardo <contato.leoo0@gmail.com>
2023-06-16 14:51:41 -0300

    feat: adicionado funcionalidade de adicionar alugueis no histórico do cliente.

--- PartyManager.WinApp/ModuloAluguel/ControladorAluguel.cs
+++ PartyManager.WinApp/ModuloAluguel/ControladorAluguel.cs
@@ -4,134 +4,135 @@ using PartyManager.Dominio.ModuloFesta;
 
 namespace PartyManager.WinApp.ModuloAluguel
 {
-     public class ControladorAluguel : ControladorBase
-     {
-          private IRepositorioAluguel repoAluguel;
-          private IRepositorioFesta repoFesta;
-          private IRepositorioCliente repoCliente;
-          private TabelaAluguelControl tabelaAluguel;
+    public class ControladorAluguel : ControladorBase
+    {
+        private IRepositorioAluguel repoAluguel;
+        private IRepositorioFesta repoFesta;
+        private IRepositorioCliente repoCliente;
+        private TabelaAluguelControl tabelaAluguel;
 
-          public ControladorAluguel(IRepositorioAluguel repositorioAluguel, IRepositorioFesta repositorioFesta, IRepositorioCliente repositorioCliente)
-          {
-               repoAluguel = repositorioAluguel;
-               repoFesta = repositorioFesta;
-               repoCliente = repositorioCliente;
-          }
+        public ControladorAluguel(IRepositorioAluguel repositorioAluguel, IRepositorioFesta repositorioFesta, IRepositorioCliente repositorioCliente)
+        {
+            repoAluguel = repositorioAluguel;
+            repoFesta = repositorioFesta;
+            repoCliente = repositorioCliente;
+        }
 
-          public override string ToolTipInserir => "Inserir Aluguel";
+        public override string ToolTipInserir => "Inserir Aluguel";
 
-          public override string ToolTipEditar => "Editar Aluguel";
+        public override string ToolTipEditar => "Editar Aluguel";
 
-          public override string ToolTipDeletar => "Deletar Aluguel";
+        public override string ToolTipDeletar => "Deletar Aluguel";
 
-          public override void Deletar()
-          {
-               Aluguel aluguelSelecionado = ObterAluguelSelecionado();
+        public override void Deletar()
+        {
+            Aluguel aluguelSelecionado = ObterAluguelSelecionado();
 
-               if (aluguelSelecionado == null)
-               {
-                    MessageBox.Show($"Selecione um aluguel primeiro!",
-                        "Exclusão de Aluguéis",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Exclamation);
+            if (aluguelSelecionado == null)
+            {
+                MessageBox.Show($"Selecione um aluguel primeiro!",
+                    "Exclusão de Aluguéis",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
 
-                    return;
-               }
+                return;
+            }
 
-               DialogResult opcaoEscolhida = MessageBox.Show($"Deseja excluir o aluguel do cliente {aluguelSelecionado.festa.cliente.nome}?", "Exclusão de Aluguéis",
-                   MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            DialogResult opcaoEscolhida = MessageBox.Show($"Deseja excluir o aluguel do cliente {aluguelSelecionado.festa.cliente.nome}?", "Exclusão de Aluguéis",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
 
-               if (opcaoEscolhida == DialogResult.OK)
-                    repoAluguel.Deletar(aluguelSelecionado);
+            if (opcaoEscolhida == DialogResult.OK)
+                repoAluguel.Deletar(aluguelSelecionado);
 
-               CarregarAlugueis();
+            CarregarAlugueis();
 
-               if (opcaoEscolhida == DialogResult.OK)
-                    TelaPrincipalForm.Instancia.AtualizarRodape($"Tema deletado com sucesso!", TipoStatusEnum.Sucesso);
-          }
+            if (opcaoEscolhida == DialogResult.OK)
+                TelaPrincipalForm.Instancia.AtualizarRodape($"Tema deletado com sucesso!", TipoStatusEnum.Sucesso);
+        }
 
-          public override void Editar()
-          {
-               TelaAluguelForm telaAluguel = new TelaAluguelForm();
-               Aluguel aluguelSelecionado = ObterAluguelSelecionado();
+        public override void Editar()
+        {
+            TelaAluguelForm telaAluguel = new TelaAluguelForm();
+            Aluguel aluguelSelecionado = ObterAluguelSelecionado();
 
-               if (aluguelSelecionado == null)
-               {
-                    MessageBox.Show($"Selecione um aluguel primeiro!",
-                        "Edição de Aluguéis",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Exclamation);
-                    return;
-               }
+            if (aluguelSelecionado == null)
+            {
+                MessageBox.Show($"Selecione um aluguel primeiro!",
+                    "Edição de Aluguéis",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                return;
+            }
 
-               List<Cliente> ListaCompletaCliente = repoCliente.SelecionarTodos();
-               List<Festa> ListaCompletaFesta = repoFesta.SelecionarTodos();
-               telaAluguel.PopularComboBox(ListaCompletaCliente, ListaCompletaFesta);
-               telaAluguel.Text = "Edição de Aluguéis";
-               telaAluguel.ConfigurarTela(aluguelSelecionado);
+            List<Cliente> ListaCompletaCliente = repoCliente.SelecionarTodos();
+            List<Festa> ListaCompletaFesta = repoFesta.SelecionarTodos();
+            telaAluguel.PopularComboBox(ListaCompletaCliente, ListaCompletaFesta);
+            telaAluguel.Text = "Edição de Aluguéis";
+            telaAluguel.ConfigurarTela(aluguelSelecionado);
 
-               DialogResult opcaoEscolhida = telaAluguel.ShowDialog();
-               if (opcaoEscolhida == DialogResult.OK)
-               {
-                    Aluguel aluguelAtualizado = telaAluguel.ObterAluguel();
- 
-                    repoAluguel.Editar(aluguelSelecionado.id, aluguelAtualizado);
+            DialogResult opcaoEscolhida = telaAluguel.ShowDialog();
+            if (opcaoEscolhida == DialogResult.OK)
+            {
+                Aluguel aluguelAtualizado = telaAluguel.ObterAluguel();
 
-               }
+                repoAluguel.Editar(aluguelSelecionado.id, aluguelAtualizado);
 
-               CarregarAlugueis();
+            }
 
-               if (opcaoEscolhida == DialogResult.OK)
-                    TelaPrincipalForm.Instancia.AtualizarRodape($"Aluguel editado com sucesso!", TipoStatusEnum.Sucesso);
-          }
+            CarregarAlugueis();
 
-          public override void Inserir()
-          {
-               List<Cliente> ListaCompletaCliente = repoCliente.SelecionarTodos();
-               List<Festa> ListaCompletaFesta = repoFesta.SelecionarTodos();
+            if (opcaoEscolhida == DialogResult.OK)
+                TelaPrincipalForm.Instancia.AtualizarRodape($"Aluguel editado com sucesso!", TipoStatusEnum.Sucesso);
+        }
 
-               TelaAluguelForm telaAluguel = new TelaAluguelForm();
-               telaAluguel.PopularComboBox(ListaCompletaCliente, ListaCompletaFesta);
+        public override void Inserir()
+        {
+            List<Cliente> ListaCompletaCliente = repoCliente.SelecionarTodos();
+            List<Festa> ListaCompletaFesta = repoFesta.SelecionarTodos();
 
-               DialogResult opcaoEscolhida = telaAluguel.ShowDialog();
-               if (opcaoEscolhida == DialogResult.OK)
-               {
-                    Aluguel aluguel = telaAluguel.ObterAluguel();
-                    repoAluguel.Inserir(aluguel);
-               }
+            TelaAluguelForm telaAluguel = new TelaAluguelForm();
+            telaAluguel.PopularComboBox(ListaCompletaCliente, ListaCompletaFesta);
 
-               CarregarAlugueis();
+            DialogResult opcaoEscolhida = telaAluguel.ShowDialog();
+            if (opcaoEscolhida == DialogResult.OK)
+            {
+                Aluguel aluguel = telaAluguel.ObterAluguel();
+                aluguel.festa.cliente.alugueis.Add(aluguel);
+                repoAluguel.Inserir(aluguel);
+            }
 
-               if (opcaoEscolhida == DialogResult.OK)
-                    TelaPrincipalForm.Instancia.AtualizarRodape($"Aluguel inserido com sucesso!", TipoStatusEnum.Sucesso);
-          }
+            CarregarAlugueis();
 
-          private Aluguel ObterAluguelSelecionado()
-          {
-               int id = tabelaAluguel.ObterIdSelecionado();
-               return repoAluguel.SelecionarPorId(id);
-          }
+            if (opcaoEscolhida == DialogResult.OK)
+                TelaPrincipalForm.Instancia.AtualizarRodape($"Aluguel inserido com sucesso!", TipoStatusEnum.Sucesso);
+        }
 
-          public void CarregarAlugueis()
-          {
-               List<Aluguel> alugueis = repoAluguel.SelecionarTodos();
-               tabelaAluguel.AtualizarRegistros(alugueis);
-               TelaPrincipalForm.Instancia.AtualizarRodape($"Visualizando {alugueis.Count} aluguel/alguéis", TipoStatusEnum.Visualizando);
-          }
+        private Aluguel ObterAluguelSelecionado()
+        {
+            int id = tabelaAluguel.ObterIdSelecionado();
+            return repoAluguel.SelecionarPorId(id);
+        }
 
-          public override UserControl ObterListagem()
-          {
-               if (tabelaAluguel == null)
-                    tabelaAluguel = new TabelaAluguelControl();
+        public void CarregarAlugueis()
+        {
+            List<Aluguel> alugueis = repoAluguel.SelecionarTodos();
+            tabelaAluguel.AtualizarRegistros(alugueis);
+            TelaPrincipalForm.Instancia.AtualizarRodape($"Visualizando {alugueis.Count} aluguel/alguéis", TipoStatusEnum.Visualizando);
+        }
 
-               CarregarAlugueis();
+        public override UserControl ObterListagem()
+        {
+            if (tabelaAluguel == null)
+                tabelaAluguel = new TabelaAluguelControl();
 
-               return tabelaAluguel;
-          }
+            CarregarAlugueis();
 
-          public override string ObterTipoCadastro()
-          {
-               return "Cadastros de Aluguéis";
-          }
-     }
+            return tabelaAluguel;
+        }
+
+        public override string ObterTipoCadastro()
+        {
+            return "Cadastros de Aluguéis";
+        }
+    }
 }
