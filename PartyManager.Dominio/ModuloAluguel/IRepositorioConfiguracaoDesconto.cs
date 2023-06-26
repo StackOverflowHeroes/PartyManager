94d2b05c1590d4f35e819d3b1a34686c7800fe5f
Leonardo <contato.leoo0@gmail.com>
2023-06-26 09:29:58 -0300

    feat: implementado os arquivos necessários e suas funcionalidades.

---
+++ PartyManager.Dominio/ModuloAluguel/IRepositorioConfiguracaoDesconto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace PartyManager.Dominio.ModuloAluguel
+{
+    public interface IRepositorioConfiguracaoDesconto
+    {
+        void GravarConfiguracoesDesconto(ConfiguracaoDesconto configuracaoDesconto);
+        ConfiguracaoDesconto ObterConfiguracaoDeDesconto();
+    }
+}
